3f423ac09c6596fc6b9fb3341033f114345a8af6
gaoyan666 <gaoyan666@DESKTOP-R98657R>
2018-09-22 12:17:18 +0800

    1111

--- git_demo_test1/src/com/zd/test/Demo2.java
+++ git_demo_test1/src/com/zd/test/Demo2.java
@@ -1,9 +1,12 @@
 package com.zd.test;
 
+import org.omg.Messaging.SyncScopeHelper;
+
 public class Demo2 {
 
 	
 	public static void main(String[] args) {
 		System.out.println(11111);
+		System.out.print("gaoyan");
 	}
 }
