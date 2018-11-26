1206f3245038e263c8c627cd56966368eadee689
wangcz <wangcz>
2018-11-26 12:48:04 +0800

    第一次提交

--- idea_java_demo_git/src/com/dzkj/test/TestDemo1.java
+++ idea_java_demo_git/src/com/dzkj/test/TestDemo1.java
@@ -3,5 +3,6 @@ package com.dzkj.test;
 public class TestDemo1 {
     public static void main(String[] args) {
         System.out.println("idea java git");
+        System.out.println("你好 idea git");
     }
 }
