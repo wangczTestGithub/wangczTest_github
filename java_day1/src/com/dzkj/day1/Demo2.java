885f24cd77afe5429ad661fba86575e75e2f5b49
wangcz <wangcz>
2018-11-26 21:38:14 +0800

    Merge branch 'master' of https://github.com/wangczTestGithub/wangczTest_github
    # Conflicts:
    #	java_day1/src/com/dzkj/day1/Demo2.java

--- java_day1/src/com/dzkj/day1/Demo2.java
+++ java_day1/src/com/dzkj/day1/Demo2.java
@@ -6,7 +6,7 @@ public class Demo2 {
         System.out.println("谭大大");
         System.out.println("王大大");
         System.out.println("王王大大");
-        System.out.println(111);
         System.out.println("罗罗大大");
+        System.out.println(111);
     }
 }
