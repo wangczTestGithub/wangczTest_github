57998e2a6cf984dcd8112b7478f72deb6f7af387
wangcz <wangcz>
2018-11-26 13:58:33 +0800

    修改

--- idea_java_demo_git/src/com/dzkj/test/TestDemo1.java
+++ idea_java_demo_git/src/com/dzkj/test/TestDemo1.java
@@ -3,6 +3,6 @@ package com.dzkj.test;
 public class TestDemo1 {
     public static void main(String[] args) {
         System.out.println("你好 idea git");
-        System.out.println();
+        System.out.println("工作空间2");
     }
 }
