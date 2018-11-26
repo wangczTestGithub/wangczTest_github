4208b8da688941cc1af560780c036323d2a07706
wangcz <wangcz>
2018-11-26 20:45:53 +0800

    第一个java项目提交

--- java_day1/src/com/dzkj/day1/Demo1.java
+++ java_day1/src/com/dzkj/day1/Demo1.java
@@ -6,5 +6,6 @@ package com.dzkj.day1;
 public class Demo1 {
     public static void main(String[] args) {
         System.out.println("Hello World!!!");
+        System.out.println("大旗英雄传");
     }
 }
