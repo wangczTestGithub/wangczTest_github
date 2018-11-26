10c6ce059d310e2c4eeb597437739c99683b4ba7
wangcz <wangcz>
2018-11-26 20:39:57 +0800

    第一个java项目提交

---
+++ java_day1/src/com/dzkj/day1/Demo1.java
@@ -0,0 +1,10 @@
+package com.dzkj.day1;
+
+/**
+ * java第一个程序：Helllo World
+ */
+public class Demo1 {
+    public static void main(String[] args) {
+        System.out.println("Hello World!!!");
+    }
+}
